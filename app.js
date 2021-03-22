deda092a9057758849bcbeeccce5dac42677764e
Aziz Iskandar <aziziskandar@zoho.com>
2021-03-22 23:14:11 +0800

    Added arrow function

--- app.js
+++ app.js
@@ -4,51 +4,77 @@ document.querySelector('.button3').addEventListener('click', getAPIData);
 
 function getText() {
   fetch('test.txt')
-    .then(function(res){
-      return res.text();
-    })
-    .then(function(data){
-      document.querySelector('.output').innerHTML = data;
-    })
-    .catch(function(err){
-      console.log(err);
-    });
+    // .then(function(res){
+    //   return res.text();
+    // })
+    .then(res => res.text())
+
+    // .then(function(data){
+    //   document.querySelector('.output').innerHTML = data;
+    // })
+    .then(data => document.querySelector('.output').innerHTML = data)
+
+    // .catch(function(err){
+    //   console.log(err);
+    // });
+    .catch(err => console.log(err));
 }
 
 function getJSON() {
   fetch('posts.json')
-    .then(function(res){
-      return res.json();
-    })
-    .then(function(posts){
+    // .then(function(res){
+    //   return res.json();
+    // })
+    .then(res => res.json())
+
+    // .then(function(posts){
+    //   let postOutput = '';
+    //   posts.forEach(function(post){
+    //     postOutput += `
+    //         <li>${post.title}</li>
+    //     `
+    //   });
+    //   document.querySelector('.output').innerHTML = postOutput;
+    // })
+    .then(posts => {
       let postOutput = '';
-      posts.forEach(function(post){
+      posts.forEach(post => {
         postOutput += `
-            <li>${post.title}</li>
+        <li>${post.title}</li>
         `
       });
-
       document.querySelector('.output').innerHTML = postOutput;
     })
-    .catch(function(err){
-      console.log(err);
-    })
+
+    // .catch(function(err){
+    //   console.log(err);
+    // })
+    .catch(err => console.log(err));
 }
 
 function getAPIData() {
   fetch('https://api.github.com/users')
-    .then(function(res) {
-      return res.json();
-    })
-    .then(function(users){
-      let userOutput = '';
-      users.forEach(function(user){
-        userOutput += `<li>${user.login}</li>`
-      }); 
+    // .then(function(res) {
+    //   return res.json();
+    // })
+    .then(res => res.json())
 
+    // .then(function(users){
+    //   let userOutput = '';
+    //   users.forEach(function(user){
+    //     userOutput += `<li>${user.login}</li>`
+    //   }); 
+
+    //   document.querySelector('.output').innerHTML = userOutput;
+    // })
+    .then(users => {
+      let userOutput = '';
+      users.forEach(user => userOutput += `<li>${user.login}</li>`);
       document.querySelector('.output').innerHTML = userOutput;
     })
-    .catch(function(err){
-      console.log(err);
-    });
+
+    // .catch(function(err){
+    //   console.log(err);
+    // });
+    .catch(err => console.log(err));
 }
